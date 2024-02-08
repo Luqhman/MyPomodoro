2ed0ab1320bfdfdbcfe5c9f7d362e63b1106c3c0
Muhamad Luqhman Hakim Bin Abdul Jalil <BRLHBAJMU@slake.sssb.com>
2024-02-08 16:44:12 +0800

    login page

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class LoginPage extends StatelessWidget {
@@ -7,11 +8,29 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
+      appBar: AppBar(),
       body: Column(
         children: [
-          Text("This is login page"),
-          Text("This is still login page"),
+          Center(child: Image.network("https://services.garmin.com/appsLibraryBusinessServices_v0/rest/apps/4b11ad8f-3e48-4112-83df-336065c49829/icon/d31ca7be-0497-4370-801f-97a29dc3b584", height: 200, width: 200)),
+          Text("MyPomodoro", style: GoogleFonts.abrilFatface(textStyle: TextStyle(color: Colors.red, fontSize: 35)),),
+          SizedBox(height: 50,),
+          Text("Don't be busy, be productive", style: GoogleFonts.dmSerifDisplay(textStyle: TextStyle(color: Colors.cyan[900], fontSize: 24)) ,),
+          SizedBox(height: 130,),
+          SizedBox(
+              height: 60,
+              width: 300,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.cyan[900],
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                ),
+                onPressed:(){}, child: Text("Enter", style: TextStyle(fontSize: 25, color:Colors.white)),)
+          )
+
+
+
+
         ],
       ),
     );
