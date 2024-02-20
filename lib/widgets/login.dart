d5a85dc99b923f58ad8bcc1542e253df5c81785b
Muhamad Luqhman Hakim Bin Abdul Jalil <BRLHBAJMU@slake.sssb.com>
2024-02-20 10:22:24 +0800

    done login gui

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pomodoro/widgets/home.dart';
 
 
 class LoginPage extends StatelessWidget {
@@ -11,11 +13,11 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(),
       body: Column(
         children: [
-          Center(child: Image.network("https://services.garmin.com/appsLibraryBusinessServices_v0/rest/apps/4b11ad8f-3e48-4112-83df-336065c49829/icon/d31ca7be-0497-4370-801f-97a29dc3b584", height: 200, width: 200)),
+          Center(child: Image.network("https://services.garmin.com/appsLibraryBusinessServices_v0/rest/apps/4b11ad8f-3e48-4112-83df-336065c49829/icon/d31ca7be-0497-4370-801f-97a29dc3b584", height: 300, width: 200)),
           Text("MyPomodoro", style: GoogleFonts.abrilFatface(textStyle: TextStyle(color: Colors.red, fontSize: 35)),),
-          SizedBox(height: 50,),
+          SizedBox(height: 30,),
           Text("Don't be busy, be productive", style: GoogleFonts.dmSerifDisplay(textStyle: TextStyle(color: Colors.cyan[900], fontSize: 24)) ,),
-          SizedBox(height: 130,),
+          SizedBox(height: 110,),
           SizedBox(
               height: 60,
               width: 300,
@@ -25,7 +27,9 @@ class LoginPage extends StatelessWidget {
                   elevation: 3,
                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
                 ),
-                onPressed:(){}, child: Text("Enter", style: TextStyle(fontSize: 25, color:Colors.white)),)
+                onPressed:(){
+                   Navigator.push(context, MaterialPageRoute(builder: (content)=> HomePage()));
+                }, child: Text("Enter", style: TextStyle(fontSize: 25, color:Colors.white)),)
           )
 
 
